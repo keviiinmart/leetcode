a854672231ba28bcbdd1ed52072cfd96325afabd
Kevin Martinez <57552248+keviiinmart@users.noreply.github.com>
2024-05-15 15:53:52 -0500

    Create RemoveElement.py

---
+++ RemoveElement.py
@@ -0,0 +1,9 @@
+#27. Remove Element
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if val != nums[i]:
+                nums[k] = nums[i]
+                k+=1
+        return k
